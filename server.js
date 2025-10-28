5a8bca4694c5ba0ba805e34fa0558d0739e60f47
vijayasaradhi979 <162435479+vijayasaradhi979@users.noreply.github.com>
2025-10-28 17:31:24 +0530

    change package json

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express=require("express")
 require('dotenv').config();
+const express=require("express")
 const app =express()
 const connectToDb=require('../auth/database/db')
 const authRoutes=require('../auth/routes/route')
